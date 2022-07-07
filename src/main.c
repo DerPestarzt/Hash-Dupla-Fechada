9f8f4a5b147e6bfe7bca7ef2b25b7f8f8c0332eb
Gabriel Marcondes <56900319+DerPestarzt@users.noreply.github.com>
2022-07-07 19:26:26 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,31 +3,31 @@
 int main(){
 	HashTable h;
 	int M = 41;
-
+	int cont = 0;
 	
 	Initialize(&h, M);
-
-	Insert(&h, 64, 1001);
-	Insert(&h, 11, 1002);
-	Insert(&h, 20, 1003);
-	Insert(&h, 53, 1004);
-	Insert(&h, 27, 1005);
-	Insert(&h, 7, 1006);
-	Insert(&h, 8, 1007);
-	Insert(&h, 9, 1008);
-	Insert(&h, 65, 1001);
-	Insert(&h, 12, 1002);
-	Insert(&h, 21, 1003);
-	Insert(&h, 54, 1004);
-	Insert(&h, 28, 1005);
-	Insert(&h, 81, 1006);
-	Insert(&h, 87, 1007);
-	Insert(&h, 91, 1008);
-	Insert(&h, 29, 1005);
-	Insert(&h, 35, 1006);
-	Insert(&h, 13, 1007);
-	Insert(&h, 88, 1008);
-	Insert(&h, 23, 1008);
+	
+	Insert(&h, 64, 1001, &cont);
+	Insert(&h, 11, 1002, &cont);
+	Insert(&h, 20, 1003, &cont);
+	Insert(&h, 53, 1004, &cont);
+	Insert(&h, 27, 1005, &cont);
+	Insert(&h, 7, 1006, &cont);
+	Insert(&h, 8, 1007, &cont);
+	Insert(&h, 9, 1008, &cont);
+	Insert(&h, 65, 1001, &cont);
+	Insert(&h, 12, 1002, &cont);
+	Insert(&h, 21, 1003, &cont);
+	Insert(&h, 54, 1004, &cont);
+	Insert(&h, 28, 1005, &cont);
+	Insert(&h, 81, 1006, &cont);
+	Insert(&h, 87, 1007, &cont);
+	Insert(&h, 91, 1008, &cont);
+	Insert(&h, 29, 1005, &cont);
+	Insert(&h, 35, 1006, &cont);
+	Insert(&h, 13, 1007, &cont);
+	Insert(&h, 88, 1008, &cont);
+	Insert(&h, 23, 1008, &cont);
 
 	
 
@@ -35,6 +35,8 @@ int main(){
 
 	printf("\n\n");
 	Imprime(&h);
+	printf("\n\n");
+	printf("Contador de Conflitos: %d\n", cont);
 
 
 	return 0;

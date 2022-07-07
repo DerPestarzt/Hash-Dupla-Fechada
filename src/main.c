92e465d0b263032629cc9fd04d366e5b5e555f93
Gabriel Marcondes <56900319+DerPestarzt@users.noreply.github.com>
2022-07-07 09:57:06 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,27 +2,32 @@
 
 int main(){
 	HashTable h;
-	int M = 8;
+	int M = 41;
 
 	
 	Initialize(&h, M);
 
-	Insert(&h, 64, 1000);
-	Insert(&h, 11, 1001);
-	Insert(&h, 20, 1002);
-	Insert(&h, 53, 1003);
-	Insert(&h, 27, 1004);
-	Insert(&h, 7, 1005);
-	Insert(&h, 8, 1005);
-	Insert(&h, 9, 1005);
-	
-
-
-	
-	// printf("\n\n");
-	// printf("Value:%d\n", getValue(&h, 20));
-	// printf("Value:%d\n", getValue(&h, 30));
-	// printf("Value:%d\n", getValue(&h, 10));
+	Insert(&h, 64, 1001);
+	Insert(&h, 11, 1002);
+	Insert(&h, 20, 1003);
+	Insert(&h, 53, 1004);
+	Insert(&h, 27, 1005);
+	Insert(&h, 7, 1006);
+	Insert(&h, 8, 1007);
+	Insert(&h, 9, 1008);
+	Insert(&h, 65, 1001);
+	Insert(&h, 12, 1002);
+	Insert(&h, 21, 1003);
+	Insert(&h, 54, 1004);
+	Insert(&h, 28, 1005);
+	Insert(&h, 81, 1006);
+	Insert(&h, 87, 1007);
+	Insert(&h, 91, 1008);
+	Insert(&h, 29, 1005);
+	Insert(&h, 35, 1006);
+	Insert(&h, 13, 1007);
+	Insert(&h, 88, 1008);
+	Insert(&h, 23, 1008);
 
 	
 
